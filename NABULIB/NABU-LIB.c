7b947897c4cec8445dca9788fcc78d1aab5a9035
Alexander G. M. Smith <agmsmith@ncf.ca>
2024-12-28 17:51:13 -0500

    Added (void) to empty function arguments, that's all.

--- NABULIB/NABU-LIB.c
+++ NABULIB/NABU-LIB.c
@@ -15,7 +15,7 @@
 // ------
 // **************************************************************************
 
-void initNABULib() {
+void initNABULib(void) {
 
   // Turn off the rom
   IO_CONTROL = CONTROL_ROMSEL | CONTROL_VDOBUF;
@@ -106,7 +106,7 @@ void initNABULib() {
   initNABULIBAudio();
 }
 
-void initNABULIBAudio() {
+void initNABULIBAudio(void) {
 
   // Noise envelope
   ayWrite(6, 0b00000000);
@@ -124,20 +124,20 @@ void initNABULIBAudio() {
   ayWrite(13, 0);
 }
 
-void nop() {
+void nop(void) {
   __asm
     NOP
   __endasm;
 }
 
-void NABU_DisableInterrupts() {
+void NABU_DisableInterrupts(void) {
 
   __asm
     di
   __endasm;
 }
 
-void NABU_EnableInterrupts() {
+void NABU_EnableInterrupts(void) {
 
   __asm
     ei
@@ -172,7 +172,7 @@ void RightShift(uint8_t *arr, uint16_t len, uint8_t n) {
 // **************************************************************************
 
 #ifndef DISABLE_HCCA_RX_INT  
-  void isrHCCARX() __naked {
+  void isrHCCARX(void) __naked {
 
     // review LIS and only bc, hl, a registers are used for this function
     __asm
@@ -209,7 +209,7 @@ void RightShift(uint8_t *arr, uint16_t len, uint8_t n) {
 #endif 
 
 #ifndef DISABLE_KEYBOARD_INT
-  void isrKeyboard() __naked {
+  void isrKeyboard(void) __naked {
 
     __asm
       push bc;
@@ -272,17 +272,17 @@ void RightShift(uint8_t *arr, uint16_t len, uint8_t n) {
   // ------------------
   // **************************************************************************
 
-  void vt_clearToEndOfScreen() {
+  void vt_clearToEndOfScreen(void) {
 
     printf("%cJ", 27);
   }
 
-  void vt_clearToEndOfLine() {
+  void vt_clearToEndOfLine(void) {
 
     printf("%cK", 27);
   }
 
-  void vt_clearScreen() {
+  void vt_clearScreen(void) {
 
     if (_EMULATION_MODE == 0)
       putchar(26);
@@ -290,17 +290,17 @@ void RightShift(uint8_t *arr, uint16_t len, uint8_t n) {
       printf("%cE", 27);
   }
 
-  void vt_clearLine() {
+  void vt_clearLine(void) {
 
     printf("%cl", 27);
   }
 
-  void vt_clearToStartOfLine() {
+  void vt_clearToStartOfLine(void) {
 
     printf("%co", 27);
   }
 
-  void vt_clearToStartOfScreen() {
+  void vt_clearToStartOfScreen(void) {
 
     printf("%cd", 27);
   }
@@ -314,7 +314,7 @@ void RightShift(uint8_t *arr, uint16_t len, uint8_t n) {
         printf("%cB", 27);
   }
 
-  void vt_cursorHome() {
+  void vt_cursorHome(void) {
 
     printf("%cH", 27);
   }
@@ -346,7 +346,7 @@ void RightShift(uint8_t *arr, uint16_t len, uint8_t n) {
         printf("%cA", 27);
   }
 
-  void vt_deleteLine() {
+  void vt_deleteLine(void) {
 
     if (_EMULATION_MODE)
       printf("%cR", 27);
@@ -366,7 +366,7 @@ void RightShift(uint8_t *arr, uint16_t len, uint8_t n) {
     putchar(32 + x);
   }
 
-  void vt_insertLine() {
+  void vt_insertLine(void) {
 
     if (_EMULATION_MODE == 0)
       printf("%cE", 27);
@@ -374,7 +374,7 @@ void RightShift(uint8_t *arr, uint16_t len, uint8_t n) {
       printf("%cL", 27);
   }
 
-  void vt_restoreCursorPosition() {
+  void vt_restoreCursorPosition(void) {
 
     if (_EMULATION_MODE == 0)
       printf("%ck", 27);
@@ -389,37 +389,37 @@ void RightShift(uint8_t *arr, uint16_t len, uint8_t n) {
     putchar(color);
   }
 
-  void vt_saveCursorPosition() {
+  void vt_saveCursorPosition(void) {
 
     printf("%cj", 27);
   }
 
-  void vt_cursorUpAndInsert() {
+  void vt_cursorUpAndInsert(void) {
 
     printf("%cI", 27);
   }
 
-  void vt_wrapOff() {
+  void vt_wrapOff(void) {
 
     printf("%cw", 27);
   }
 
-  void vt_wrapOn() {
+  void vt_wrapOn(void) {
 
     printf("%cv", 27);
   }
 
-  void vt_normalVideo() {
+  void vt_normalVideo(void) {
 
     printf("%cq", 27);
   }
 
-  void vt_reverseVideo() {
+  void vt_reverseVideo(void) {
 
     printf("%cp", 27);
   }
 
-  bool isCloudCPM() {
+  bool isCloudCPM(void) {
 
     return _CLOUD_CPM_KEY == 0x55;
   }
@@ -502,12 +502,12 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
 
 #ifndef DISABLE_KEYBOARD_INT
 
-  uint8_t isKeyPressed() {
+  uint8_t isKeyPressed(void) {
 
     return (_kbdBufferWritePos != _kbdBufferReadPos);
   }
 
-  uint8_t getChar() {
+  uint8_t getChar(void) {
 
     #if defined(DISABLE_CURSOR) || defined(DISABLE_VDP)
 
@@ -601,7 +601,7 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
   // ------------
   // **************************************************************************
 
-  bool hcca_ping() {
+  bool hcca_ping(void) {
 
     hcca_writeByte(0xa1);
 
@@ -623,12 +623,12 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     return false;
   }
 
-  bool hcca_isRxBufferAvailable() {
+  bool hcca_isRxBufferAvailable(void) {
 
     return _rxBufferWritePos != _rxBufferReadPos;
   }
 
-  uint8_t hcca_getSizeOfDataInBuffer() {
+  uint8_t hcca_getSizeOfDataInBuffer(void) {
 
     if (_rxBufferReadPos > _rxBufferWritePos)
       return (0xff - _rxBufferReadPos) + _rxBufferWritePos;
@@ -636,7 +636,7 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     return _rxBufferWritePos - _rxBufferReadPos;
   }
 
-  uint8_t hcca_readByte() {
+  uint8_t hcca_readByte(void) {
 
     while (_rxBufferWritePos == _rxBufferReadPos);
 
@@ -647,26 +647,26 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     return ret;
   }
 
-  uint16_t hcca_readUInt16() {
+  uint16_t hcca_readUInt16(void) {
 
     return (uint16_t)hcca_readByte() +
            ((uint16_t)hcca_readByte() << 8);
   }
 
-  int16_t hcca_readInt16() {
+  int16_t hcca_readInt16(void) {
 
     return (int16_t)hcca_readByte() +
            ((int16_t)hcca_readByte() << 8);
   }
 
-  uint32_t hcca_readUInt32() {
+  uint32_t hcca_readUInt32(void) {
 
     uint8_t ret[4] = { hcca_readByte(), hcca_readByte(), hcca_readByte(), hcca_readByte() };
 
     return *((uint32_t *)ret);
   }
 
-  int32_t hcca_readInt32() {
+  int32_t hcca_readInt32(void) {
 
     uint8_t ret[4] = { hcca_readByte(), hcca_readByte(), hcca_readByte(), hcca_readByte() };
 
@@ -791,7 +791,7 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     IO_VDPLATCH = (address >> 8);
   }
 
-  void waitVdpISR() __naked {
+  void waitVdpISR(void) __naked {
 
     __asm
 
@@ -813,7 +813,7 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     __endasm;
   }
 
-  void vdp_enableVDPReadyInt() {
+  void vdp_enableVDPReadyInt(void) {
 
     NABU_DisableInterrupts();
 
@@ -837,7 +837,7 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     vdp_setRegister(1, _vdpReg1Val | 0b00100000 );   
   }
 
-  void vdp_waitVDPReadyInt() {
+  void vdp_waitVDPReadyInt(void) {
 
     // uncomment this to enable debugging for the VDP to see if the vdpIsReady flag was
     // set prior to your program calling vdp_waitVDPReadyInt(). That means your program took
@@ -860,7 +860,7 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     while (!vdpIsReady);
   }
 
-  void vdp_disableVDPReadyInt() {
+  void vdp_disableVDPReadyInt(void) {
 
     vdp_setRegister(1, _vdpReg1Val);
 
@@ -881,7 +881,7 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     NABU_EnableInterrupts();
   }
 
-  void vdp_addISR(void (*isr)()) {
+  void vdp_addISR(void (*isr)(void)) {
 
     NABU_DisableInterrupts();
 
@@ -907,7 +907,7 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     vdp_setRegister(1, _vdpReg1Val | 0b00100000 );   
   }
   
-  void vdp_removeISR() {
+  void vdp_removeISR(void) {
 
     vdp_setRegister(1, _vdpReg1Val);
 
@@ -1043,7 +1043,7 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     vdp_clearScreen();
   }
 
-  void vdp_clearVRAM() {
+  void vdp_clearVRAM(void) {
 
     vdp_setWriteAddress(0x00);
 
@@ -1081,12 +1081,12 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     vdp_init(VDP_MODE_G2, 0, bgColor, bigSprites, scaleSprites, autoScroll, splitThirds);
   }
 
-  void vdp_initMultiColorMode() {
+  void vdp_initMultiColorMode(void) {
 
     vdp_init(VDP_MODE_MULTICOLOR, 0, 0, false, false, false, false);
   }
 
-  void vdp_clearScreen() {
+  void vdp_clearScreen(void) {
       
     vdp_setWriteAddress(_vdpPatternNameTableAddr);
 
@@ -1610,7 +1610,7 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     }
   }
 
-  void vdp_newLine() {
+  void vdp_newLine(void) {
 
     if (vdp_cursor.y == _autoScrollBottomRow) {
 
@@ -1717,7 +1717,7 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     _vdp_textBuffer[y * _vdpCursorMaxXFull + x] = c;
   }
 
-  void vdp_refreshViewPort() {
+  void vdp_refreshViewPort(void) {
 
     vdp_setWriteAddress(_vdpPatternNameTableAddr);
 
@@ -1851,3 +1851,4 @@ void playNoteDelay(uint8_t channel, uint8_t note, uint16_t delayLength) {
     vdp_print(str);
   }
 #endif
+
